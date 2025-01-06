d572d14538b372655529bf7862161d8499728ed9
Hangyul Yi <hangyulyi3@gmail.com>
2025-01-05 19:00:14 -0700

    add throwable probablity chance

--- entities/good/star_0/homeless_man.gd
+++ entities/good/star_0/homeless_man.gd
@@ -18,12 +18,30 @@ func _physics_process(delta):
 		
 		
 func throw():
-	spawn_projectile()
+	var selected_throwable = pick_random_throwable()
+	print("picked throwable: ", selected_throwable)
+	spawn_projectile(selected_throwable)
 	for body in vision.get_overlapping_bodies():
 		if body.is_in_group("Enemy"):
 			body.queue_free()
 
-func spawn_projectile():
+func pick_random_throwable() -> String:
+	var total_chance = 0.0
+	for chance in throwables.values():
+		total_chance += chance
+		
+	var random_value = randf() * total_chance
+	var cumulative_chance = 0.0
+	
+	for throwable in throwables.keys():
+		cumulative_chance += throwables[throwable]
+		if random_value <= cumulative_chance:
+			return throwable
+			
+	# In case of error, default to the first throwable (banana)
+	return throwables.keys()[0]
+
+func spawn_projectile(throwable):
 	var projectile = preload("res://entities/components/projectile.tscn")
 	var projectile_instance = projectile.instantiate()
 	get_parent().add_child(projectile_instance)
